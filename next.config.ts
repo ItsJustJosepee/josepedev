896acfefc85aa82c49f4370a1ab251e89c2a6eee
José Barajas <83657397+ItsJustJosepee@users.noreply.github.com>
2025-09-15 21:20:48 -0600

    Quitar config.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    allowedDevOrigins: ['http://192.168.100.6:3000'], // tu IP + puerto
-  },
+  
 };
 
 export default nextConfig;
