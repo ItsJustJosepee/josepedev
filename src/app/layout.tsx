5bdd67544fd7cda4a940792ff2e1c2a0cbe1b6d2
José Barajas <83657397+ItsJustJosepee@users.noreply.github.com>
2025-09-16 12:31:12 -0600

    SpeedInsights para Vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import LocalFont from "next/font/local";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const Minecraft = LocalFont({
   src: [
@@ -36,7 +37,7 @@ export default function RootLayout({
         className={`${Minecraft.variable} ${Pusab.variable} antialiased`}
       >
         <Header />
-        <main>{children}<Analytics /></main>
+        <main>{children}<SpeedInsights /><Analytics /></main>
         <Footer />
       </body>
     </html>
